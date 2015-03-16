0c2d3a94efd68168cb55f91e42d4e546e072a5b0
rektide <rektide@voodoowarez.com>
2015-03-15 23:41:05 -0400

    Watch "Select Special Key" select box, don't add if in use.

--- add-chord-express.user.js
+++ add-chord-express.user.js
@@ -12,7 +12,7 @@
 
 // The web page editor makes you manually click from step, and this script automates the process:
 // 1. Click 'add new chord' button
-// 2. Click the key mapping text area to focus on it
+// 2. Click the chord mapping text area to focus on it
 // 3. Type your desired output
 // 4. Click the chord you want to map from
 // 5. Click 'apply' to set
@@ -29,19 +29,16 @@ var el= {
   add: "form:addRowButton2",
   edit: "form:chordsDataTable:0:editChordButtonId",
   mapping: "keyMappingForm:keyMapping",
+  insertSpecial: "keyMappingForm:insertSpecialKeyCommandButton",
   apply: "keyMappingForm:applyChord"
 }
 for(var i in el){
 	var id= el[i]
 	el[i]= document.getElementById.bind(document, id)
 }
-
-// Things seemed dire sometime back then
-//function doClick(el){
-//	var click= document.createEvent("MouseEvents");
-//	click.initEvent("click", true, true);
-//	el.dispatchEvent(click)
-//}
+el.specialSelect = function(){
+	return document.getElementById("keyMappingForm:keyMappingPanel").querySelector("select")
+}
 
 // edit the first chord in the chords table, focus on in it's mapping
 function editChord(){
@@ -54,7 +51,18 @@ function editChord(){
 function delayedAddChord(){
 	setTimeout(editChord, DELAY)
 }
+// next time we would add a chord, don"t
+// some ui controls (insert special char) will cause reloads that we don't want to immediately 
+// begin editing a new chord-mapping in
+function skipNextAdd(){
+	GM_setValue("skipNextAdd", 1)
+	GM_log("skipping next add:"+GM_getValue("skipNextAdd"))
+}
 
+// Cleanup the special character, which if present would skip our addBlock on start
+function clearSpecial(){	
+	el.specialSelect().value = ""
+}
 // if we do anything else, it'll blow up our binding by repainting over all controls
 // keep rebinding the controls we care about
 function bindAll(){
@@ -63,14 +71,18 @@ function bindAll(){
 	// so remove possible bindings, set one binding
 	add.removeEventListener("click", delayedAddChord)
 	add.addEventListener("click", delayedAddChord)
+
+	// This codepath doesnt work in Scriptish- GM_setValue doesn't seem to endure reloads
+	//var insertSpecial= el.insertSpecial()
+	//insertSpecial.removeEventListener("click", skipNextAdd)
+	//insertSpecial.addEventListener("click", skipNextAdd)
+
+	var apply = el.apply()
+	apply.removeEventListener("click", clearSpecial)
+	apply.addEventListener("click", clearSpecial)
 }
 setInterval(bindAll, 350)
 bindAll()
-
-// add a chord as soon as page starts. We kind of have to do this, since 'apply'
-// jumps to a new page and we need _something_ to get us back to making the next
-// mapping
-el.add().click();
 
 // Control-Space
 // Apply the current in progress mapping, advance to next to add
@@ -79,3 +91,16 @@ document.addEventListener('keydown', function(e){
 	e.preventDefault()
 	el.apply().click()
 })
+
+// add a chord as soon as page starts. We kind of have to do this, since 'apply'
+// jumps to a new page and we need _something_ to get us back to making the next
+// mapping
+var skip = GM_getValue("skipNextAdd") || el.specialSelect().value
+if(!skip){
+	GM_log("add:"+skip)
+	el.add().click()
+}else{
+	// unless we're told not to 
+	GM_log("we skipped:"+skip)
+	GM_deleteValue("skipNextAdd")
+}
